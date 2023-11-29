45cd78aad6ef862f0c73cc66649b7ef0d021093f
Doyeong Lee <doyoung413@gmail.com>
2023-11-30 00:28:04 +0900

    Change texInedx swap method in verticesDemo

--- Game/source/VerticesDemo.cpp
+++ Game/source/VerticesDemo.cpp
@@ -16,10 +16,14 @@ void VerticesDemo::Init()
 	Engine::Engine().GetVKRenderManager()->LoadQuad(glm::vec4(0.f, 0.f, 1.f, 1.f), 0.f);
 	Engine::Engine().GetVKRenderManager()->LoadLineQuad(glm::vec4(0.f, 0.f, 1.f, 1.f));
 	Engine::Engine().GetVKRenderManager()->LoadLineQuad(glm::vec4(0.f, 0.f, 1.f, 1.f));
+	Engine::Engine().GetVKRenderManager()->LoadQuad(glm::vec4(1.f, 0.f, 1.f, 1.f), 1.f);
 
 	auto matrices = Engine::Engine().GetVKRenderManager()->GetMatrices();
 	(*matrices)[0].texIndex = 0.f;
 	(*matrices)[1].texIndex = 1.f;
+	glm::mat4 modelMatrix(1.0f);
+	(*matrices)[5].model = glm::scale(modelMatrix, { 0.5f, 0.5f, 0.f });
+	(*matrices)[5].texIndex = 0.f;
 }
 
 void VerticesDemo::Update(float /*dt*/)
@@ -27,7 +31,7 @@ void VerticesDemo::Update(float /*dt*/)
 	auto matrices = Engine::Engine().GetVKRenderManager()->GetMatrices();
 	//(*matrices)[0].model = glm::mat4(1);
 	//(*matrices)[0].texIndex = 0.f;
-	(*matrices)[0].model = glm::translate((*matrices)[0].model, {0.01f, 0.f, 0.f});
+	(*matrices)[0].model = glm::translate((*matrices)[0].model, { 0.01f, 0.f, 0.f });
 	if ((*matrices)[0].model[3][0] > 2.f)
 	{
 		(*matrices)[0].model = glm::mat4(1);
@@ -44,15 +48,6 @@ void VerticesDemo::Update(float /*dt*/)
 
 	glm::mat4 modelMatrix(1.0f);
 	(*matrices)[1].model = glm::translate(modelMatrix, { 1.f,1.f,0.f });
-	switch (static_cast<int>((*matrices)[1].texIndex))
-	{
-	case 0:
-		(*matrices)[1].texIndex = 1.f;
-		break;
-	case 1:
-		(*matrices)[1].texIndex = 0.f;
-		break;
-	}
 
 	glm::mat4 modelMatrix2(1.0f);
 	(*matrices)[2].model = glm::translate(modelMatrix2, { -1.f,1.f,0.f });
@@ -63,6 +58,28 @@ void VerticesDemo::Update(float /*dt*/)
 
 	glm::mat4 modelMatrix4(1.0f);
 	(*matrices)[4].model = glm::translate(modelMatrix4, { 1.f,-1.f,0.f });
+
+	if (Engine::Instance().GetInputManager()->IsKeyPressedOnce(KEYBOARDKEYS::Z))
+	{
+		glm::mat4 modelMatrix5(1.0f);
+		(*matrices)[5].texIndex = 1.f;
+		(*matrices)[5].model = glm::scale(modelMatrix5, { 0.5f, 0.5f, 0.f });
+	}
+	else if (Engine::Instance().GetInputManager()->IsKeyPressedOnce(KEYBOARDKEYS::X))
+	{
+		glm::mat4 modelMatrix5(1.0f);
+		(*matrices)[5].texIndex = 0.f;
+		(*matrices)[5].model = glm::scale(modelMatrix5, { 0.5f, 0.5f, 0.f });
+	}
+
+	if (Engine::Instance().GetInputManager()->IsKeyPressedOnce(KEYBOARDKEYS::C))
+	{
+		(*matrices)[1].texIndex = 1.f;
+	}
+	else if (Engine::Instance().GetInputManager()->IsKeyPressedOnce(KEYBOARDKEYS::V))
+	{
+		(*matrices)[1].texIndex = 0.f;
+	}
 }
 
 void VerticesDemo::Draw(float /*dt*/)
