775892cb0f5c796a10bea3dc758a3093165a95f0
minjae-yu <minjae.yoo@digipen.edu>
2024-02-11 22:07:26 +0900

    Fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -5,23 +5,24 @@
 
 #include "VerticesDemo.hpp"
 #include "PocketBallDemo/PocketBallDemo.hpp"
-
+#include "PlatformDemo/PlatformDemo.hpp"
 
 #undef main
 
 int main(void)
 {
-	Engine& engine = Engine::Instance();
-	engine.Init("Vulkan Demo", 1280, 720, false, WindowMode::NORMAL);
-	engine.SetFPS(FrameRate::FPS_60);
+    Engine& engine = Engine::Instance();
+    engine.Init("Vulkan Demo", 1280, 720, false, WindowMode::NORMAL);
+    engine.SetFPS(FrameRate::FPS_60);
 
-	engine.GetSoundManager()->LoadMusicFilesFromFolder(L"..\\Game\\assets\\Musics");
-	engine.GetSoundManager()->LoadSoundFilesFromFolder("../Game/assets/Sounds");
+    engine.GetSoundManager()->LoadMusicFilesFromFolder(L"..\Game\assets\Musics");
+    engine.GetSoundManager()->LoadSoundFilesFromFolder("../Game/assets/Sounds");
 
-	engine.GetGameStateManager()->AddLevel(new VerticesDemo);
-	engine.GetGameStateManager()->AddLevel(new PocketBallDemo);
-	engine.GetGameStateManager()->LevelInit(GameLevel::POCKETBALL);
+    engine.GetGameStateManager()->AddLevel(new VerticesDemo);
+    engine.GetGameStateManager()->AddLevel(new PocketBallDemo);
+    engine.GetGameStateManager()->AddLevel(new PlatformDemo);
+    engine.GetGameStateManager()->LevelInit(GameLevel::PLATFORMDEMO);
 
-	engine.Update();
-	engine.End();
+    engine.Update();
+    engine.End();
 }
