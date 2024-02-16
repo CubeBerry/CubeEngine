e6f23cb02e506879109a9c5217742b6f935686f6
minjae-yu <minjae.yoo@digipen.edu>
2024-02-16 12:41:19 +0900

    Fixed Culling For 2D Flipping

--- Engine/common_vk/source/VKPipeLine.cpp
+++ Engine/common_vk/source/VKPipeLine.cpp
@@ -168,7 +168,8 @@ void VKPipeLine::InitPipeLine(VkShaderModule* vertexModule, VkShaderModule* frag
 		break;
 	}
 	//Culling
-	rasterizationStateInfo.cullMode = VK_CULL_MODE_BACK_BIT;
+	//rasterizationStateInfo.cullMode = VK_CULL_MODE_BACK_BIT;
+	rasterizationStateInfo.cullMode = VK_CULL_MODE_NONE;
 	rasterizationStateInfo.lineWidth = 1.0f;
 
 	//Create Multisample State Info(MSAA)
