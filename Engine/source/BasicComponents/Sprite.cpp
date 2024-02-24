46b2fa0fe3a31aa7a446c6de38824761d8cab647
minjae-yu <minjae.yoo@digipen.edu>
2024-02-24 17:40:38 +0900

    Minor Code Fix

--- Engine/source/BasicComponents/Sprite.cpp
+++ Engine/source/BasicComponents/Sprite.cpp
@@ -31,7 +31,7 @@ void Sprite::Update(float dt)
 	UpdateView();
 	UpdateModel(GetOwner()->GetPosition(), GetOwner()->GetSize(), GetOwner()->GetRotate());
 
-	if (currAnim >= 0 && currAnim < !animations[currAnim]->IsAnimationDone())
+	if (currAnim >= 0 && !animations[currAnim]->IsAnimationDone())
 	{
 		animations[currAnim]->Update(dt);
 		Engine::Instance().GetVKRenderManager()->GetVertexVector()->at(materialId).frameSize = glm::vec4(GetFrameSize() / textureSize, 0.f, 0.f);
