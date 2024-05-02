d1a9a2951ceb6f274d6f1f1f583ce902b7c792c2
minjae-yu <minjae.yoo@digipen.edu>
2024-05-02 16:45:05 +0900

    Integrated VulkanSDK

---
+++ Engine/common_vk/libs/VulkanSDK/1.3.268.0/Include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.28.4-0-gcc016b004 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.28.4-0-gcc016b004"
+#endif
