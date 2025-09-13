9bb5684ab1c16584fd19441ea098bde3a3714e6a
rapt0r <ymjgood11@gmail.com>
2025-09-13 11:29:01 -0700

    Fixed DX12 Compute Barriers Issue
    - m_renderTarget, DXRenderTarget, m_postProcessTexture's barrier should be changed so that it can be used for next frame.

--- Engine/common_dx/source/DXComputeBuffer.cpp
+++ Engine/common_dx/source/DXComputeBuffer.cpp
@@ -49,7 +49,7 @@ void DXComputeBuffer::InitComputeBuffer(
 	DXHelper::ThrowIfFailed(device->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&m_computeRootSignature)));
 	DXHelper::ThrowIfFailed(m_computeRootSignature->SetName(L"Compute Root Signature"));
 
-	// Create Pipeline State Object
+	// Compile Compute Shader
 #ifdef _DEBUG
 	UINT compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
 #else
@@ -87,57 +87,12 @@ void DXComputeBuffer::InitComputeBuffer(
 	psoDesc.CS = CD3DX12_SHADER_BYTECODE(computeShader.Get());
 	psoDesc.Flags = D3D12_PIPELINE_STATE_FLAG_NONE;
 
-	// Create the Pipeline State Object (PSO)
+	// Create Pipeline State Object (PSO)
 	DXHelper::ThrowIfFailed(device->CreateComputePipelineState(&psoDesc, IID_PPV_ARGS(&m_computePipelineState)));
 	DXHelper::ThrowIfFailed(m_computePipelineState->SetName(L"Compute PSO"));
 
-	//m_texture.Reset();
-
-	auto textureDesc = CD3DX12_RESOURCE_DESC::Tex2D(
-		DXGI_FORMAT_R8G8B8A8_UNORM,
-		m_width,
-		m_height,
-		1, 0, 1, 0,
-		D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS
-	);
-
-	CD3DX12_HEAP_PROPERTIES heapProps(D3D12_HEAP_TYPE_DEFAULT);
-	DXHelper::ThrowIfFailed(device->CreateCommittedResource(
-		&heapProps,
-		D3D12_HEAP_FLAG_NONE,
-		&textureDesc,
-		D3D12_RESOURCE_STATE_COMMON,
-		nullptr,
-		IID_PPV_ARGS(&m_postProcessTexture)
-	));
-	m_postProcessTexture->SetName(L"Post Process Output Texture");
-
-	UINT descriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV);
-	CD3DX12_CPU_DESCRIPTOR_HANDLE srvHeapStart(srvHeap->GetCPUDescriptorHandleForHeapStart());
-	CD3DX12_GPU_DESCRIPTOR_HANDLE srvGpuHeapStart(srvHeap->GetGPUDescriptorHandleForHeapStart());
-
-	constexpr int postProcessDescriptorOffset = 505;
-
-	m_postProcessInputSrvCpuHandle = srvHeapStart;
-	m_postProcessInputSrvCpuHandle.Offset(postProcessDescriptorOffset, descriptorSize);
-	m_postProcessInputSrvGpuHandle = srvGpuHeapStart;
-	m_postProcessInputSrvGpuHandle.Offset(postProcessDescriptorOffset, descriptorSize);
-
-	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
-	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
-	srvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2DMS;
-	device->CreateShaderResourceView(renderTarget->GetMSAARenderTarget().Get(), &srvDesc, m_postProcessInputSrvCpuHandle);
-
-	m_postProcessOutputUavCpuHandle = srvHeapStart;
-	m_postProcessOutputUavCpuHandle.Offset(postProcessDescriptorOffset + 1, descriptorSize);
-	m_postProcessOutputUavGpuHandle = srvGpuHeapStart;
-	m_postProcessOutputUavGpuHandle.Offset(postProcessDescriptorOffset + 1, descriptorSize);
-
-	D3D12_UNORDERED_ACCESS_VIEW_DESC uavDesc = {};
-	uavDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	uavDesc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
-	device->CreateUnorderedAccessView(m_postProcessTexture.Get(), nullptr, &uavDesc, m_postProcessOutputUavCpuHandle);
+	// Initialize output texture, descriptors
+	OnResize(device, m_width, m_height, srvHeap, renderTarget);
 }
 
 void DXComputeBuffer::OnResize(
@@ -155,7 +110,7 @@ void DXComputeBuffer::OnResize(
 		DXGI_FORMAT_R8G8B8A8_UNORM,
 		m_width,
 		m_height,
-		1, 0, 1, 0,
+		1, 1, 1, 0,
 		D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS
 	);
 
@@ -227,6 +182,13 @@ void DXComputeBuffer::PostProcess(
 
 	commandList->CopyResource(renderTarget.Get(), m_postProcessTexture.Get());
 
-	CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(renderTarget.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_RENDER_TARGET);
-	commandList->ResourceBarrier(1, &barrier);
+	//CD3DX12_RESOURCE_BARRIER barrier = CD3DX12_RESOURCE_BARRIER::Transition(renderTarget.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_RENDER_TARGET);
+	//commandList->ResourceBarrier(1, &barrier);
+
+	CD3DX12_RESOURCE_BARRIER finalBarriers[] = {
+		CD3DX12_RESOURCE_BARRIER::Transition(renderTarget.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_RENDER_TARGET),
+		CD3DX12_RESOURCE_BARRIER::Transition(dxRenderTarget->GetMSAARenderTarget().Get(), D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE, D3D12_RESOURCE_STATE_RESOLVE_SOURCE),
+		CD3DX12_RESOURCE_BARRIER::Transition(m_postProcessTexture.Get(), D3D12_RESOURCE_STATE_COPY_SOURCE, D3D12_RESOURCE_STATE_COMMON)
+	};
+	commandList->ResourceBarrier(_countof(finalBarriers), finalBarriers);
 }
